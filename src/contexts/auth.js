064431c44273df1953cd2189eec44839700a6bda
Diney Oliveira <diney.oliveira>
2023-04-11 16:51:42 -0300

    criando o contexto

---
+++ src/contexts/auth.js
@@ -0,0 +1,19 @@
+import React, { createContext, useState } from "react";
+
+export const AuthContext = createContext({});
+
+function AuthProvider({children}){
+
+    const [user, setUser] = useState({
+        nome: 'Diney'
+    })
+
+    return(
+        <AuthContext.Provider value={ user}>
+            {children}
+        </AuthContext.Provider>
+        
+    );
+}
+
+export default AuthProvider;
