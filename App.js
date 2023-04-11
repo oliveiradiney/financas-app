a707772871cd86f10c56473842dee665ebddd447
Diney Oliveira <diney.oliveira>
2023-04-11 13:53:13 -0300

    criando a tela de SignIn

--- App.js
+++ App.js
@@ -1,21 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
+
+import 'react-native-gesture-handler';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import {Text, View, StatusBar } from 'react-native';
+import firebase from './src/services/firebaseConnection';
+import { LogBox } from 'react-native';
+
+import Routes from './src/routes';
 
 export default function App() {
+
+  LogBox.ignoreAllLogs();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <StatusBar backgroundColor='#131313' barStyle="light" />
+      <Routes />
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/services/firebaseConnection.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+
+let firebaseConfig = {
+    apiKey: "AIzaSyBAiWkggXxF0O4HGnnqkAVJut4S0iQqoEU",
+    authDomain: "financas-595fa.firebaseapp.com",
+    databaseURL: "https://financas-595fa-default-rtdb.firebaseio.com",
+    projectId: "financas-595fa",
+    storageBucket: "financas-595fa.appspot.com",
+    messagingSenderId: "62416506148",
+    appId: "1:62416506148:web:7d17814c234cd2645df7b1"
+};
+
+if (!firebase.apps.length) {
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+}
+
+export default firebase;
