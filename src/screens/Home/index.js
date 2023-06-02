f21dfd2c0050746ef69c424b365a1681293494fe
Diney Oliveira <diney.oliveira>
2023-06-02 15:00:11 -0300

    mostrando registros

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,10 +1,11 @@
-import React, {useContext, useState} from 'react';
+import React, {useContext, useState, useEffect} from 'react';
 
 import { View, Text, Button} from 'react-native';
 import { AuthContext } from '../../contexts/auth';
 import Header from '../../components/Header';
+import { format } from 'date-fns';
 import HistoricoList from '../../components/HistoricoList';
-
+import firebase from '../../services/firebaseConnection';
 import {
     Background,
     Container,
@@ -16,20 +17,41 @@ import {
 
 export default function Home({navigation}){
     const { user, signOut } = useContext(AuthContext);
+    const [historico , setHistorico] = useState([]);
+    const [ saldo, setSaldo ] = useState(0);
 
-    const [historico, setHistorico ] = useState([
-        {key: '1', tipo:'receita', valor: 1200},
-        {key: '2', tipo:'despesa', valor: 200},
-        {key: '3', tipo:'receita', valor: 40},
-        {key: '4', tipo:'receita', valor: 89.62},
-        {key: '5', tipo:'despesa', valor: 520},
-        {key: '6', tipo:'despesa', valor: 310},
-        {key: '7', tipo:'receita', valor: 1200},
-        {key: '8', tipo:'despesa', valor: 200},
-        {key: '9', tipo:'receita', valor: 40},
-        {key: '10', tipo:'receita', valor: 89.62},
-        {key: '11', tipo:'despesa', valor: 520},
-    ])
+  
+    const uid = user && user.uid;
+
+    useEffect(() => {
+        async function loadlist(){
+            await firebase.database().ref('users').child(uid).on('value', (snapshot) => {
+                setSaldo(snapshot.val().saldo);
+            });
+
+            await firebase.database().ref('historico')
+            .child(uid)
+            .orderByChild('date').equalTo(format(new Date, 'dd/MM/yy'))
+            .limitToLast(10).on('value', (snapshot) => {
+                setHistorico([]);
+
+                snapshot.forEach((childItem) => {
+                    let list = {
+                        key: childItem.key,
+                        tipo: childItem.val().tipo,
+                        valor: childItem.val().valor
+                    };
+
+                    setHistorico(oldArray => [...oldArray, list].reverse());
+                });
+
+                
+            })
+        }
+
+        loadlist();
+    }, []);
+   
     return(
        <Background>
             <Header />
@@ -37,7 +59,7 @@ export default function Home({navigation}){
                 <Nome>
                     {user && user.nome}
                 </Nome>
-                <Saldo>R$ 200</Saldo>
+                <Saldo>R$ {saldo.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.')}</Saldo>
             </Container>
 
             <Title>Ultimas movimentações</Title>
--- src/screens/New/index.js
+++ src/screens/New/index.js
@@ -36,6 +36,7 @@ export default function New() {
                 }
             ]
         )
+        
     }
 
     async function handleAdd(){
@@ -59,6 +60,7 @@ export default function New() {
         });
 
         setValor('');
+        setTipo('')
         Keyboard.dismiss();
 
         navigation.navigate('Home')
